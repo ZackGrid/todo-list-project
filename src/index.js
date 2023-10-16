be52fc68cdf1726a11fce872305dab04319d20d5
Zack <110679912+ZackGrid@users.noreply.github.com>
2023-10-16 18:01:58 -0300

    Get everything up to date

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import { format } from "date-fns";
-
-const content = document.querySelector('.content');
-
-const today = new Date();
-const formattedDate = format(today, 'PPPP');
-
-content.textContent = formattedDate;
+import './style.css';
